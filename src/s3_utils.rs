1244b3f2f3167f4531f3057a6524fefce80d74fb
Ninad Bhangui <nmb@pop-os.localdomain>
2021-07-11 19:21:45 +0530

    init

---
+++ src/s3_utils.rs
@@ -0,0 +1,17 @@
+use s3::{Region, output::ListObjectsV2Output};
+use std::error::Error;
+
+pub async fn get_objects(bucket: &str) -> Result<ListObjectsV2Output, Box<dyn Error>> {
+    let conf = s3::Config::builder()
+        .region(Region::new("us-east-2"))
+        .build();
+
+    let client = s3::Client::from_conf(conf);
+    let resp = client.list_objects_v2().bucket(bucket).send().await?;
+    Ok(resp)
+    // println!("{:?}", resp);
+    // for object in resp.contents.unwrap_or_default() {
+    //     println!("{:?}", object.key.unwrap_or_default());
+    // }
+    // Ok(())
+}
